de7ce33327dd0b238509c277f74caaff9a290652
Xamenyap <jpham2811@gmail.com>
2021-07-15 21:26:04 +0700

    Remove other students' solutions, add my own solutions, add go mod support

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,8 +13,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if url, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, url, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +40,20 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var paths []pathToURL
+	if err := yaml.Unmarshal(yml, &paths); err != nil {
+		return nil, err
+	}
+
+	pathsToURLs := make(map[string]string)
+	for _, path := range paths {
+		pathsToURLs[path.Path] = path.URL
+	}
+
+	return MapHandler(pathsToURLs, fallback), nil
+}
+
+type pathToURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
